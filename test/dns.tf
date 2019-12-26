79d0b59755b95731567e6d8a58d6d48ce12c79e7
shztki <shztki@gmail.com>
2019-12-26 21:17:33 +0900

    disable

--- test/dns.tf
+++ test/dns.tf
@@ -5,36 +5,36 @@ resource "sakuracloud_dns" "dns" {
   tags        = [var.my_account, var.my_domain]
 }
 
-resource "sakuracloud_dns_record" "record_server" {
-  dns_id = sakuracloud_dns.dns.id
-  count  = var.server["count"]
-  name   = element(sakuracloud_server.server.*.name, count.index)
-  type   = "A"
-  ttl    = 300
-  value  = element(sakuracloud_server.server.*.ipaddress, count.index)
-}
-
-resource "sakuracloud_dns_record" "record_server02" {
-  dns_id = sakuracloud_dns.dns.id
-  count  = var.server02["count"]
-  name   = element(sakuracloud_server.server02.*.name, count.index)
-  type   = "A"
-  ttl    = 300
-  value  = element(sakuracloud_server.server02.*.ipaddress, count.index)
-}
-
-resource "sakuracloud_dns_record" "record_gslb" {
-  dns_id = sakuracloud_dns.dns.id
-  name   = "@"
-  type   = "ALIAS"
-  ttl    = 300
-  value  = format("%s.", sakuracloud_gslb.gslb.fqdn)
-}
-
-resource "sakuracloud_dns_record" "record_elb" {
-  dns_id = sakuracloud_dns.dns.id
-  name   = "elb"
-  type   = "CNAME"
-  ttl    = 300
-  value  = format("%s.", sakuracloud_proxylb.foobar.fqdn)
-}
+#resource "sakuracloud_dns_record" "record_server" {
+#  dns_id = sakuracloud_dns.dns.id
+#  count  = var.server["count"]
+#  name   = element(sakuracloud_server.server.*.name, count.index)
+#  type   = "A"
+#  ttl    = 300
+#  value  = element(sakuracloud_server.server.*.ipaddress, count.index)
+#}
+#
+#resource "sakuracloud_dns_record" "record_server02" {
+#  dns_id = sakuracloud_dns.dns.id
+#  count  = var.server02["count"]
+#  name   = element(sakuracloud_server.server02.*.name, count.index)
+#  type   = "A"
+#  ttl    = 300
+#  value  = element(sakuracloud_server.server02.*.ipaddress, count.index)
+#}
+#
+#resource "sakuracloud_dns_record" "record_gslb" {
+#  dns_id = sakuracloud_dns.dns.id
+#  name   = "@"
+#  type   = "ALIAS"
+#  ttl    = 300
+#  value  = format("%s.", sakuracloud_gslb.gslb.fqdn)
+#}
+#
+#resource "sakuracloud_dns_record" "record_elb" {
+#  dns_id = sakuracloud_dns.dns.id
+#  name   = "elb"
+#  type   = "CNAME"
+#  ttl    = 300
+#  value  = format("%s.", sakuracloud_proxylb.foobar.fqdn)
+#}
